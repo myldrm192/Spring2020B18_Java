acd413a7e2432cd3dd39c03163d90e7359a535db
myldrm192 <myldrm192@gmail.com>
2020-05-13 01:27:49 -0400

    day37_Constructor

---
+++ src/day37_Constructor/Data.java
@@ -0,0 +1,20 @@
+package day37_Constructor;
+
+public class Data {
+
+    static int staticVariable = 300;
+    int instanceVariable = 400;
+
+    public static void staticMethod() {
+
+        System.out.println("Static Method");
+
+    }
+
+    public void instanceMethod(){
+
+        System.out.println("instance method");
+    }
+
+
+}
